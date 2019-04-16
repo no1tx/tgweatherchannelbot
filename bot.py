c4468821b42d751762cc691cd9eb6d5fa7d82625
Pavel Isaev (no1_tx) <no1@no1sg.ru>
2019-04-16 12:11:38 +0300

    Add report type to message
    Signed-off-by: Pavel Isaev (no1_tx) <no1@no1sg.ru>

--- bot.py
+++ bot.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
-from metar import Metar
-from apscheduler.schedulers.blocking import BlockingScheduler
 from metar.Metar import Metar
 import requests
 import json
 import config
 import telebot
+import time
 
 bot = telebot.TeleBot(config.token)
 code = config.code
 channel = config.channel
+global sendtimestamp
 
 
-def fetch_and_decode_metar(metar_code):
+def fetch_and_decode_metar(code):
     global decoded_data
-    link = 'http://metartaf.ru/' + metar_code + '.json'
+    link = 'http://metartaf.ru/' + code + '.json'
     print(f'Send request to {link}')
     response = requests.get(link)
     parsed_response = json.loads(response.text)
     print('JSON load')
+    city = parsed_response["name"]
     metar_data = parsed_response["metar"]
     print(f'METAR DATA: {metar_data}')
     metar_data = metar_data.split('\n')
@@ -27,10 +28,10 @@ def fetch_and_decode_metar(metar_code):
 
 
 def send():
-    global stimestamp
-    if timestamp != stimestamp:
+    global sendtimestamp
+    if timestamp != sendtimestamp:
         bot.send_message(chat_id=channel, text=message)
-        stimestamp = timestamp
+        sendtimestamp = timestamp
     else:
         print('Message is same, stop')
 
@@ -42,25 +43,25 @@ def create_message():
     timestamp = str(decoded_data.time)
     time = 'Время измерения: ' + str(decoded_data.time) + '\n'
     if not decoded_data.present_weather():
-        weather = 'Погода: clear\n'
+        weather = 'Погода: Ясно\n'
     else:
         weather = 'Погода: ' + decoded_data.present_weather() + '\n'
+    report = decoded_data.report_type()
     temp = 'Температура: ' + str(decoded_data.temp) + '\n'
-    dew_point = 'Точка росы:' + str(decoded_data.dewpt) + '\n'
+    dew_point = 'Точка росы: ' + str(decoded_data.dewpt) + '\n'
     wind = 'Ветер: ' + decoded_data.wind(units="KMH") + '\n'
     visibility = 'Видимость: ' + decoded_data.visibility() + '\n'
     pressure = 'Давление: ' + str(decoded_data.press) + '\n'
     sky = 'Небо: ' + decoded_data.sky_conditions() + '\n'
-    message = time + weather + temp + wind + dew_point + visibility + pressure + sky + 'Источник данных: metartaf.ru'
+    message = time + report + weather + temp + wind + dew_point + visibility + pressure + sky + 'Источник данных: metartaf.ru'
 
 
-def create_schedule():
-    scheduler = BlockingScheduler()
-    scheduler.add_job(send, 'interval', hours=1)
-    scheduler.start()
-
-
-fetch_and_decode_metar(code)  # for initial starting
+sendtimestamp = ''
+fetch_and_decode_metar(code)
 create_message()
 send()
-create_schedule()
+while True:
+    fetch_and_decode_metar(code)
+    create_message()
+    send()
+    time.sleep(60)
